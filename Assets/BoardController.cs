f62595332f911556124b645cda5fccdbd9bff2e0
TuPham <tupd@athena.studio>
2023-08-01 19:19:28 +0700

    hp bar + die effect + round + floating text

--- Assets/BoardController.cs
+++ Assets/BoardController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class BoardController : MonoBehaviour
@@ -10,9 +11,16 @@ public class BoardController : MonoBehaviour
     [SerializeField] private List<HeroController> _redTeam;
 
     [SerializeField] private List<HeroController> _turnFight;
+    [SerializeField] private FloatingText _normalDamageText;
+    [SerializeField] private FloatingText _roundText;
 
-    public int _heroAliveRedTeam;
-    public int _heroAliveBlueTeam;
+    private int _heroAliveRedTeam;
+    private int _heroAliveBlueTeam;
+
+    public int round;
+
+    public int HeroAliveRedTeam { get => _heroAliveRedTeam; set => _heroAliveRedTeam = value; }
+    public int HeroAliveBlueTeam { get => _heroAliveBlueTeam; set => _heroAliveBlueTeam = value; }
 
     private void Awake()
     {
@@ -20,20 +28,21 @@ public class BoardController : MonoBehaviour
     }
     private void Start()
     {
+        round = 0;
         InitTurnFight();
         StartCoroutine(Combat());
     }
 
     private void InitTurnFight()
     {
-        _heroAliveRedTeam = 0;
-        _heroAliveBlueTeam = 0;
+        HeroAliveRedTeam = 0;
+        HeroAliveBlueTeam = 0;
         _turnFight = new List<HeroController>();
         for (int i = 0; i < _blueTeam.Count; i++)
         {
             if (_blueTeam[i].Data.HP > 0)
             {
-                _heroAliveBlueTeam++;
+                HeroAliveBlueTeam++;
                 _turnFight.Add(_blueTeam[i]);
             }
         }
@@ -41,48 +50,68 @@ public class BoardController : MonoBehaviour
         {
             if (_redTeam[i].Data.HP > 0)
             {
-                _heroAliveRedTeam++;
+                HeroAliveRedTeam++;
                 _turnFight.Add(_redTeam[i]);
             }
         }
-        _turnFight.Sort((a, b) => a.Data.SPD - b.Data.SPD);
+        _turnFight.Sort((a, b) => b.Data.SPD - a.Data.SPD);
     }
 
     bool CheckEnd()
     {
         if (_blueTeam.Where(hero => hero.Data.HP > 0).Count() == 0)
         {
-            Debug.Log("Lose");
+            Debug.Log("Red Win");
             return true;
         }
         if (_redTeam.Where(hero => hero.Data.HP > 0).Count() == 0)
         {
-            Debug.Log("Win");
+            Debug.Log("Blue Win");
             return true;
         }
         return false;
     }
     IEnumerator Combat()
     {
+        yield return new WaitForSeconds(1f);
         InitTurnFight();
+        ShowRound(++round);
         for (int i = 0; i < _turnFight.Count(); i++)
         {
+            _normalDamageText.gameObject.SetActive(false);
             if (_turnFight[i].Data.HP > 0)
             {
                 if (_turnFight[i].Team == 0)
                 {
-                    if (_heroAliveRedTeam > 0)
+                    if (HeroAliveRedTeam > 0)
                         _turnFight[i].Attack(_redTeam.Where(hero => hero.Data.HP > 0).ToList()[0]);
                 }
                 else
                 {
-                    if (_heroAliveBlueTeam > 0)
+                    if (HeroAliveBlueTeam > 0)
                         _turnFight[i].Attack(_blueTeam.Where(hero => hero.Data.HP > 0).ToList()[0]);
                 }
             }
-            yield return new WaitForSeconds(0f);
+            yield return new WaitForSeconds(0.5f);
         }
         if (!CheckEnd())
+        {
+            _roundText.gameObject.SetActive(false);
             StartCoroutine(Combat());
+        }
+    }
+
+    public void ShowRound(int round)
+    {
+        _roundText.transform.position = Vector3.zero;
+        _roundText.GetComponentInChildren<TextMeshPro>().text = "Round " + round.ToString();
+        _roundText.gameObject.SetActive(true);
+    }
+
+    public void ShowDamage(Vector3 pos, int damage)
+    {
+        _normalDamageText.transform.position = new Vector3(pos.x, pos.y);
+        _normalDamageText.GetComponentInChildren<TextMeshPro>().text = "-" + damage.ToString();
+        _normalDamageText.gameObject.SetActive(true);
     }
 }
--- Assets/HeroController.cs
+++ Assets/HeroController.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeroController : MonoBehaviour
 {
     [SerializeField] private int _team;
     [SerializeField] private HeroData _data;
+    [SerializeField] private int _hpMax;
 
     public HeroData Data { get => _data; set => _data = value; }
     public int Team { get => _team; set => _team = value; }
 
+    private void Start()
+    {
+        _hpMax = _data.HP;
+        GetComponentInChildren<Slider>().value = 1;
+    }
     public void Attack(HeroController enemy)
     {
         enemy.GetDamage(Data.ATK);
@@ -18,16 +25,18 @@ public class HeroController : MonoBehaviour
     public void GetDamage(int damage)
     {
         Data.HP -= damage;
-        Debug.Log("Get Damage: " + damage);
+        GetComponentInChildren<Slider>().value = Data.HP*1f / _hpMax;
+        BoardController.Instance.ShowDamage(transform.position, damage);
         if(Data.HP <= 0)
         {
+            GetComponent<Image>().color = new Color32(100, 100, 100, 255);
             if(_team == 0)
             {
-                BoardController.Instance._heroAliveBlueTeam--;
+                BoardController.Instance.HeroAliveBlueTeam--;
             }
             else
             {
-                BoardController.Instance._heroAliveRedTeam--;
+                BoardController.Instance.HeroAliveRedTeam--;
             }
             Data.HP = 0;
         }
